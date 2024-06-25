f0e598cd42a66deee17447922c36803f0a09455e
Edouard Auchet <125192936+argentswagee@users.noreply.github.com>
2024-06-25 13:05:39 +0200

    Add files via upload

---
+++ parametres.js
@@ -0,0 +1,9 @@
+document.getElementById("parametre").addEventListener("click", function () {
+  window.location.href = "index.html";
+});
+window.onload("click", function () {
+    var audio = document.getElementById("back");
+    audio.play();
+  });
+
+
